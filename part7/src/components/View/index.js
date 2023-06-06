59544a7901fb29c2a03b666eaf63c75dadc95ddc
Minh Nguyen <shenlong2361996@gmail.com>
2023-06-06 20:11:23 +0300

    refactor

---
+++ part7/src/components/View/index.js
@@ -0,0 +1,6 @@
+export * from './About.jsx'
+export * from './Anecdote.jsx'
+export * from './AnecdoteList.jsx'
+export * from './CreateNew.jsx'
+export * from './Footer.jsx'
+export * from './Menu.jsx'
